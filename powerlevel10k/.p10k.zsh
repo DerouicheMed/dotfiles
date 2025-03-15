3af679b35ea87e339dc7ebac452b8e46c184f6b6
Mohamed Derouiche <47196705+DerouicheMed@users.noreply.github.com>
2025-03-15 17:52:58 +0100

    :ok_hand: IMPROVE : update dir & git colors

--- powerlevel10k/.p10k.zsh
+++ powerlevel10k/.p10k.zsh
@@ -208,19 +208,19 @@
 
   ##################################[ dir: current directory ]##################################
   # Current directory background color.
-  typeset -g POWERLEVEL9K_DIR_BACKGROUND=4
+  typeset -g POWERLEVEL9K_DIR_BACKGROUND=13
   # Default current directory foreground color.
-  typeset -g POWERLEVEL9K_DIR_FOREGROUND=254
+  typeset -g POWERLEVEL9K_DIR_FOREGROUND=237
   # If directory is too long, shorten some of its segments to the shortest possible unique
   # prefix. The shortened directory can be tab-completed to the original.
   typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
   # Replace removed segment suffixes with this symbol.
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   # Color of the shortened directory segments.
-  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=250
+  typeset -g POWERLEVEL9K_DIR_SHORTENED_FOREGROUND=237
   # Color of the anchor directory segments. Anchor segments are never shortened. The first
   # segment is always an anchor.
-  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=255
+  typeset -g POWERLEVEL9K_DIR_ANCHOR_FOREGROUND=237
   # Display anchor directory segments in bold.
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=true
   # Don't shorten directories that contain any of these files. They are anchors.
@@ -347,8 +347,8 @@
 
   #####################################[ vcs: git status ]######################################
   # Version control background colors.
-  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=35
-  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=202
+  typeset -g POWERLEVEL9K_VCS_CLEAN_BACKGROUND=36
+  typeset -g POWERLEVEL9K_VCS_MODIFIED_BACKGROUND=208
   typeset -g POWERLEVEL9K_VCS_UNTRACKED_BACKGROUND=250
   typeset -g POWERLEVEL9K_VCS_CONFLICTED_BACKGROUND=196
   typeset -g POWERLEVEL9K_VCS_LOADING_BACKGROUND=8
